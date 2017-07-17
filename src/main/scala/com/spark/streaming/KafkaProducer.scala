d3daa859052286808a54b5f9882c1e45295eb579
anlei <anlei@ihangmei.com>
2017-07-17 14:10:53 +0800

    spark streaming 基础部分

--- src/main/scala/com/spark/streaming/KafkaProducer.scala
+++ src/main/scala/com/spark/streaming/KafkaProducer.scala
@@ -1,12 +1,49 @@
 package com.spark.streaming
 
+import java.util.Properties
+
+import com.alibaba.fastjson.JSONObject
+import com.spark.config.Config
+import kafka.javaapi.producer.Producer
+import kafka.producer.{KeyedMessage, ProducerConfig}
+
 /**
   * Created by AnLei on 2017/7/17.
   */
 object KafkaProducer {
 
-  def main(args: Array[String]): Unit = {
+  private val users = Array(
+    "张三", "李四",
+    "王五", "赵六",
+    "068b746ed4620d25e26055a9f804385f", "97edfc08311c70143401745a03a50706",
+    "d7f141563005d1b5d0d3dd30138f3f62", "c8ee90aade1671a21336c721512b817a",
+    "6b67c8c700427dee7552f81f3228c927", "a95f22eabc4fd4b580c011a3161a9d9d")
 
+  def main(args: Array[String]): Unit = {
+    val topic = Config.KAFKA_TOPIC
+    val brokers = Config.KAFKA_BROKERS
+
+    val properties: Properties = new Properties
+    properties.put("metadata.broker.list", brokers)
+    properties.put("serializer.class", "kafka.serializer.StringEncoder")
+
+    val kafkaConfig = new ProducerConfig(properties)
+    val producer = new Producer[String, String](kafkaConfig)
+
+    var index = 1
+    while(true){
+      val event = new JSONObject()
+      event.put("id", index)
+      event.put("time", System.currentTimeMillis.toString)
+      event.put("count", 1)
+      index += 1
+
+      val message: KeyedMessage[String, String] = new KeyedMessage[String, String](topic, event.toString)
+      producer.send(message)
+      println(event)
+
+      Thread.sleep(200)
+    }
   }
 
 }
---
+++ src/main/scala/com/spark/streaming/StreamingDemo.scala
@@ -0,0 +1,56 @@
+package com.spark.streaming
+
+import com.alibaba.fastjson.JSON
+import com.spark.config.Config
+import com.spark.util.RedisClient
+import kafka.serializer.StringDecoder
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+/**
+  * Created by AnLei on 2017/7/17.
+  */
+object StreamingDemo {
+
+  def main(args: Array[String]): Unit = {
+    val topic = Config.KAFKA_TOPIC
+    val brokers = Config.KAFKA_BROKERS
+
+    val conf = new SparkConf().setAppName("StreamingDemo").setMaster("local[*]")
+    val ssc = new StreamingContext(conf, Seconds(5))
+    val kafkaParams = Map[String, String](
+      "metadata.broker.list" -> brokers,
+      "serializer.class" -> "kafka.serializer.StringEncoder",
+      "auto.offset.reset" -> "largest",
+      "group.id" -> "StreamingDemo")
+    val kafkaStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, Set(topic))
+
+    val events = kafkaStream.flatMap(line => {
+      println(s"line ${line}.")
+      val data = JSON.parseObject(line._2)
+      Some(data)
+    })
+
+    val counts = events.map(x => (x.getString("id"), x.getLong("count"))).reduceByKey(_ + _)
+
+    counts.foreachRDD(rdd => {
+      rdd.foreachPartition(records => {
+        records.foreach(record => {
+          try {
+            val id = record._1
+            val count = record._2
+            println(s"id ${id} to ${count}.")
+          } catch {
+            case e: Exception => println("error:" + e)
+          }
+        })
+      })
+    })
+
+    ssc.start()
+    ssc.awaitTermination()
+
+  }
+
+}
