bdd6feda2af88c89263ee898ab8ab69ab02cd44c
anlei <anlei@ihangmei.com>
2017-08-04 17:10:17 +0800

    spark streaming 基础部分

--- src/main/scala/com/spark/config/Config.scala
+++ src/main/scala/com/spark/config/Config.scala
@@ -4,7 +4,11 @@ package com.spark.config
   * Created by AnLei on 2017/5/15.
   */
 object Config {
+  val ZOOKEEPER_QUORUM: String = "ha01:2181,ha02:2181,ha03:2181"
+
   val KAFKA_BROKERS: String = "ha01:9092,ha02:9092,ha03:9092"
+  val KAFKA_TOPIC: String = "topic2"
+  val KAFKA_GROUP: String = "group2"
   val KAFKA_USER_TOPIC: String = "user"
   val KAFKA_RECO_TOPIC: String = "reco6"
 
