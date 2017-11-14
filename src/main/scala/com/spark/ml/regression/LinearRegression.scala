9d2d60205bac1229c3a950936405b87d6b1bfe67
anlei <anlei@ihangmei.com>
2017-11-14 18:24:35 +0800

    spark ml

--- src/main/scala/com/spark/ml/regression/LinearRegression.scala
+++ src/main/scala/com/spark/ml/regression/LinearRegression.scala
@@ -1,6 +1,7 @@
 package com.spark.ml.regression
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.regression.LinearRegression
 
 /**
   * Created by AnLei on 2017/11/14.
@@ -13,6 +14,28 @@ object LinearRegression {
 
     val spark = SparkSession.builder().master("local").appName(s"${this.getClass.getSimpleName}").getOrCreate()
 
+    // Load training data
+    val training = spark.read.format("libsvm")
+      .load("data/mllib/sample_linear_regression_data.txt")
+
+    val lr = new LinearRegression()
+      .setMaxIter(10)
+      .setRegParam(0.3)
+      .setElasticNetParam(0.8)
+
+    // Fit the model
+    val lrModel = lr.fit(training)
+
+    // Print the coefficients and intercept for linear regression
+    println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
+
+    // Summarize the model over the training set and print out some metrics
+    val trainingSummary = lrModel.summary
+    println(s"numIterations: ${trainingSummary.totalIterations}")
+    println(s"objectiveHistory: [${trainingSummary.objectiveHistory.mkString(",")}]")
+    trainingSummary.residuals.show()
+    println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
+    println(s"r2: ${trainingSummary.r2}")
   }
 
 }
