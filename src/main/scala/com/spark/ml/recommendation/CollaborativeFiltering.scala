8dac4a83460c95d60225d9cbf68c4d12607d7f02
anlei <anlei@ihangmei.com>
2017-12-01 10:00:44 +0800

    spark ml cf

--- src/main/scala/com/spark/ml/recommendation/CollaborativeFiltering.scala
+++ src/main/scala/com/spark/ml/recommendation/CollaborativeFiltering.scala
@@ -1,6 +1,8 @@
 package com.spark.ml.recommendation
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.recommendation.ALS
 
 /**
   * Created by AnLei on 2017/11/14.
@@ -9,9 +11,52 @@ import org.apache.spark.sql.SparkSession
   */
 object CollaborativeFiltering {
 
+  case class Rating(userId: Int, movieId: Int, rating: Float, timestamp: Long)
+
+  def parseRating(str: String): Rating = {
+    val fields = str.split("::")
+    assert(fields.size == 4)
+    Rating(fields(0).toInt, fields(1).toInt, fields(2).toFloat, fields(3).toLong)
+  }
+
   def main(args: Array[String]): Unit = {
 
     val spark = SparkSession.builder().master("local").appName(s"${this.getClass.getSimpleName}").getOrCreate()
+
+    import spark.implicits._
+    val ratings = spark.read.textFile("data/mllib/als/sample_movielens_ratings.txt")
+      .map(parseRating)
+      .toDF()
+    val Array(training, test) = ratings.randomSplit(Array(0.8, 0.2))
+
+    // Build the recommendation model using ALS on the training data
+    val als = new ALS()
+      .setMaxIter(5)
+      .setRegParam(0.01)
+      .setUserCol("userId")
+      .setItemCol("movieId")
+      .setRatingCol("rating")
+    val model = als.fit(training)
+
+    // Evaluate the model by computing the RMSE on the test data
+    // Note we set cold start strategy to 'drop' to ensure we don't get NaN evaluation metrics
+    model.setColdStartStrategy("drop")
+    val predictions = model.transform(test)
+
+    val evaluator = new RegressionEvaluator()
+      .setMetricName("rmse")
+      .setLabelCol("rating")
+      .setPredictionCol("prediction")
+    val rmse = evaluator.evaluate(predictions)
+    println(s"Root-mean-square error = $rmse")
+
+    // Generate top 10 movie recommendations for each user
+    val userRecs = model.recommendForAllUsers(10)
+    // Generate top 10 user recommendations for each movie
+    val movieRecs = model.recommendForAllItems(10)
+
+    userRecs.show(false)
+    movieRecs.show(false)
 
     spark.stop()
   }
