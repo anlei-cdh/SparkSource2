56dcccd4df19d7b3953cb93bf89b221645d3a506
anlei <anlei@ihangmei.com>
2017-12-11 11:38:03 +0800

    spark ml cf

--- src/main/scala/com/spark/ml/regression/LinearRegression.scala
+++ src/main/scala/com/spark/ml/regression/LinearRegression.scala
@@ -1,5 +1,6 @@
 package com.spark.ml.regression
 
+import org.apache.spark.ml.linalg.Vectors
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.ml.regression.LinearRegression
 
@@ -14,28 +15,40 @@ object LinearRegression {
 
     val spark = SparkSession.builder().master("local").appName(s"${this.getClass.getSimpleName}").getOrCreate()
 
-    // Load training data
-    val training = spark.read.format("libsvm")
-      .load("data/mllib/sample_linear_regression_data.txt")
+    /**
+      * 房屋售价
+      * ID，平米数，售价(万元)
+      */
+    val trainingData = Seq(
+      (1, Vectors.dense(123), 490),
+      (2, Vectors.dense(150), 630),
+      (3, Vectors.dense(49), 120),
+      (4, Vectors.dense(58), 165),
+      (5, Vectors.dense(68), 215),
+      (6, Vectors.dense(78), 265),
+      (7, Vectors.dense(87), 310),
+      (8, Vectors.dense(115), 450),
+      (9, Vectors.dense(120), 475),
+      (10, Vectors.dense(135), 550)
+    )
+    val trainingDataFrame = spark.createDataFrame(trainingData).toDF("id", "features", "label")
+    trainingDataFrame.show(false)
 
-    val lr = new LinearRegression()
+    val line = new LinearRegression()
       .setMaxIter(10)
       .setRegParam(0.3)
       .setElasticNetParam(0.8)
+    val lineModel = line.fit(trainingDataFrame)
 
-    // Fit the model
-    val lrModel = lr.fit(training)
+    /**
+      * 预测100平米房屋的售价(万元)
+      */
+    val testData = Seq(
+      (1, Vectors.dense(100))
+    )
 
-    // Print the coefficients and intercept for linear regression
-    println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
-
-    // Summarize the model over the training set and print out some metrics
-    val trainingSummary = lrModel.summary
-    println(s"numIterations: ${trainingSummary.totalIterations}")
-    println(s"objectiveHistory: [${trainingSummary.objectiveHistory.mkString(",")}]")
-    trainingSummary.residuals.show()
-    println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
-    println(s"r2: ${trainingSummary.r2}")
+    val testDataFrame = spark.createDataFrame(testData).toDF("id", "features")
+    lineModel.transform(testDataFrame).show(false)
   }
 
 }
