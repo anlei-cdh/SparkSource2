69bbf640c8b907a156f7e9054ca5e7d1414b09e5
anlei-cdh <824652977@qq.com>
2017-12-25 14:29:44 +0800

    test git

--- src/main/scala/com/spark/rdd/Union.scala
+++ src/main/scala/com/spark/rdd/Union.scala
@@ -9,14 +9,12 @@ import org.apache.spark.{SparkConf, SparkContext}
 object Union {
 
   def main(args: Array[String]): Unit = {
-
     val conf = new SparkConf().setAppName("union").setMaster("local")
     val sc = new SparkContext(conf)
     val visits = sc.parallelize(List(("index.html","1.2.3.4"),("about.html","3.4.5.6"),("index.html","1.3.3.1")))
     val pageNames = sc.parallelize(List(("index.html","Home"),("about.html","About")))
     visits.union(pageNames).foreach(println(_))
     sc.stop()
-
   }
 
 }
